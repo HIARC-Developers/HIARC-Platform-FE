73facf5d795b222e6a23891ef9e4c8cfe77fb559
이호재 <161708856+ghwo336@users.noreply.github.com>
2025-07-19 00:49:34 +0900

    feat: 하이아크logo 수정

--- apps/intra/src/ui/components/Header.tsx
+++ apps/intra/src/ui/components/Header.tsx
@@ -1,11 +1,12 @@
 import HeaderInput from './HeaderInput';
 import { Button } from '@ui/components/Button';
+import Image from 'next/image';
 
 export default function Header(): React.ReactElement {
   return (
     <header className="flex w-full items-center justify-between border-b border-gray-200 px-6 py-4 sm:px-10">
       <div className="mx-auto flex w-5/8 min-w-[600px] items-center justify-between">
-        <div className="text-[24px] font-bold">HI - ARC</div>
+        <Image src="/Logo.svg" alt="HIARC-LOGO" width={110} height={22} />
         <div className="flex items-center gap-4">
           <HeaderInput />
           <Button variant="outline" className="bg-white text-black">
